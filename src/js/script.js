1f8fb759e9ed19d41b8c035a3723f2807afb47dc
Pavel Melnik <recyclesordie94@gmail.com>
2022-03-20 18:24:30 +0300

    added swiper slider. rewrite old

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,30 @@
-import './../js/modules/slider'
-import './debug';
+import './../js/modules/slider';
+import Debug from './debug';
 import './modules/index';
 
 import Swiper from 'swiper/bundle';
 import { getOne } from './utils';
 
-window.addEventListener( 'load', function() {
-	const swiper = new Swiper('.swiper', {
-		// Optional parameters
-		direction: 'horizontal',
-		// loop: true,
+window.addEventListener('load', function () {
+  const debug = new Debug();
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // If we need pagination
+    pagination: {
+      clickable: true,
+      el: '.slider-pagination',
+      type: 'bullets'
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
 
-		// If we need pagination
-		pagination: {
-			clickable: true,
-			el: '.swiper-pagination',
-		},
-
-		// Navigation arrows
-		navigation: {
-
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-
-		// And if we need scrollbar
-		scrollbar: {
-			el: '.swiper-scrollbar',
-		},
-	});
-
-	const x = getOne('.slider__dots');
-	console.log(x, 'x')
-	console.log( swiper,'Swiper' );
-
+  const x = getOne('.slider__dots');
+  console.log(x, 'x');
+  console.log(swiper, 'Swiper');
 });
-
